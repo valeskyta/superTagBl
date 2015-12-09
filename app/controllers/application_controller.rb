d5545a1288aa6439f88776029da238ee00a1197a
Valeskyta <valeska.arquitecto@gmail.com>
2015-12-09 18:00:55 -0300

    fixing strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,3 +15,4 @@ class ApplicationController < ActionController::Base
       redirect_to root_url, :alert => exception.message
   end
 end
+
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 before_filter :configure_sign_up_params
-# before_filter :configure_account_update_params, only: [:update]
-
-  # def country
-  # end
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
 
   def update
     super
@@ -19,17 +10,13 @@ before_filter :configure_sign_up_params
   # You can put the params you want to permit in the empty array.
      def configure_sign_up_params
       #devise_parameter_sanitizer.for(:sign_up) << :email, :password, :password_confirmation
-       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :gender, :country_id, :name, :picture, :picture_cache, :bio, :passion, :travels, :nickname ) }
+       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :picture, :bio, :username ) }
 
        devise_parameter_sanitizer.for(:account_update) << :bio
-       # devise_parameter_sanitizer.for(:account_update) << :picture_cache
        devise_parameter_sanitizer.for(:account_update) << :role
        devise_parameter_sanitizer.for(:account_update) << :type
-
        devise_parameter_sanitizer.for(:account_update) << :picture
        devise_parameter_sanitizer.for(:account_update) << :username
-
-
      end
 
   # You can put the params you want to permit in the empty array.
